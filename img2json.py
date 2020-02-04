0bfceb02379bc8a53b81e459c8234e83e1348d89
Maxime Moreillon <m.moreillon@gmail.com>
2020-02-04 16:16:35 +0900

    fixed deployment

--- img2json.py
+++ img2json.py
@@ -10,20 +10,20 @@ app = Flask(__name__)
 
 @app.route('/', methods=['GET'])
 def index():
-    return "Img2Array v5"
+    return "Img2Array v6"
 
 @app.route('/img', methods=['POST'])
 def img():
     print('Received POST on /img')
 
     # Extract path from request
-    path = request.json['path']
+    #path = request.json['path']
 
     # Normalization of input
-    img_numpy = cv2.imread(path)/255
+    #img_numpy = cv2.imread(path)/255
 
     # prepare payload
-    payload = json.dumps( {'instances': [img_numpy.tolist()]} )
+    #payload = json.dumps( {'instances': [img_numpy.tolist()]} )
 
     # Send request
     return "banana"
